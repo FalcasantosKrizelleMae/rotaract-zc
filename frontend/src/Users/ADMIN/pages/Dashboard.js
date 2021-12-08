c58a50e22452eea42a26b35b1df8c41efc3efdd0
FalcasantosKrizelleMae <bg201803214@wmsu.edu.ph>
2021-12-08 17:40:56 +0800

    dashboard

--- frontend/src/Users/ADMIN/pages/Dashboard.js
+++ frontend/src/Users/ADMIN/pages/Dashboard.js
@@ -126,19 +126,19 @@ const Dashboard = () => {
       return '';
    };
 
-   const CustomTooltip = ({ active, payload, label }) => {
-      if (active && payload && payload.length) {
-         return (
-            <div className="custom-tooltip">
-               <p className="label">{`${label} : ${payload[0].value}`}</p>
-               <p className="intro">{getIntroOfPage(label)}</p>
-               <p className="desc">Anything you want can be displayed here.</p>
-            </div>
-         );
-      }
+   //  const CustomTooltip = ({ active, payload, label }) => {
+   //     if (active && payload && payload.length) {
+   //        return (
+   //           <div className="custom-tooltip">
+   //              <p className="label">{`${label} : ${payload[0].value}`}</p>
+   //              <p className="intro">{getIntroOfPage(label)}</p>
+   //              <p className="desc">Anything you want can be displayed here.</p>
+   //           </div>
+   //        );
+   //     }
 
-      return null;
-   };
+   //     return null;
+   //  };
 
    return (
       <>
